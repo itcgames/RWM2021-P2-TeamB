cd86025008196ef95e4373d05652a9d9f2981b88
HansMeulblok <hansmeulblok4@gmail.com>
2021-11-15 13:53:24 +0000

    BI-52 adde collision and damage

--- Assets/Scripts/BaseProjectile.cs
+++ Assets/Scripts/BaseProjectile.cs
@@ -2,33 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public abstract class BaseProjectile : MonoBehaviour
 {
-    public float projectileSpeed;
-    public float lifeTime;
-    public Vector2 velocity;
-    public Rigidbody2D rb;
-    private float timer;
+    [SerializeField] protected float m_ProjectileSpeed;
+    [SerializeField] protected float m_LifeTime;
+    [SerializeField] protected Vector2 m_Velocity;
+    [SerializeField] protected Rigidbody2D m_Rigidbody2D;
+    [SerializeField] protected int m_Damage;
+    private float m_Timer;
 
     public virtual void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        SetRotation(velocity);
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        SetRotation(m_Velocity);
     }
      
     public virtual void Update()
     {
-        lifeTime -= Time.deltaTime;
+        m_LifeTime -= Time.deltaTime;
 
-        if(lifeTime <= 0)
+        if(m_LifeTime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("bloon"))
+        {
+            // other.GetComponent<Bloon>().health -= dmg;
+            Destroy(this.gameObject);
+        }
+        else if(other.CompareTag("obstacle"))
         {
             Destroy(this.gameObject);
         }
     }
     public virtual void Move()
     {
-        rb.AddForce(velocity * projectileSpeed, ForceMode2D.Force);
+        m_Rigidbody2D.AddForce(m_Velocity * m_ProjectileSpeed, ForceMode2D.Force);
     }
 
     public virtual void SetRotation(Vector2 direction)
@@ -36,5 +50,4 @@ public abstract class BaseProjectile : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
-
 }
