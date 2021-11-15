0ed36d507af1dfe47662ebb0be78c213b855eea4
HansMeulblok <hansmeulblok4@gmail.com>
2021-11-15 15:37:39 +0000

    BI-53, added tests

---
+++ Assets/Tests/TestScripts/TestFire.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFire : MonoBehaviour
+{
+    public GameObject testprojectile;
+    public Vector2 dir;
+    private GameObject currentProjectile;
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            spawnProjectile(dir);
+        }        
+    }
+
+    public void spawnProjectile(Vector2 newdir)
+    {
+        currentProjectile = Instantiate(testprojectile, this.gameObject.transform.position, Quaternion.identity);
+        currentProjectile.GetComponent<TestProjectile>().Move(newdir);
+    }
+
+    public GameObject getprojectile()
+    {
+        return currentProjectile;
+    }
+}
