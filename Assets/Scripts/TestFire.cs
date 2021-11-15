e31b9eed3b7bc92f4b5746d016740f41fd50f481
HansMeulblok <hansmeulblok4@gmail.com>
2021-11-15 14:24:43 +0000

    BI-53. Fixed velocity setting

--- Assets/Scripts/TestFire.cs
+++ Assets/Scripts/TestFire.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class TestFire : MonoBehaviour
 {
     public GameObject testprojectile;
+    public Vector2 dir;
     void Update()
     {
         if(Input.GetButtonDown("Fire1"))
         {
             GameObject newProjectile = Instantiate(testprojectile, this.gameObject.transform.position, Quaternion.identity);
-            newProjectile.GetComponent<TestProjectile>().Move();
+            newProjectile.GetComponent<TestProjectile>().Move(dir);
         }        
     }
 }
