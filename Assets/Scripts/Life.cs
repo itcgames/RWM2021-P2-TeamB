bec66d1396d5ada28eb906c6563eecfd69ab4133
HansMeulblok <hansmeulblok4@gmail.com>
2021-11-22 13:51:02 +0000

    added UI

--- Assets/Scripts/Life.cs
+++ Assets/Scripts/Life.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Life : MonoBehaviour
 {
+    [SerializeField] private Text lifeText;
     [SerializeField] public int maxLives;
     [HideInInspector] public int currentLives;
 
-    void start()
+    void Start()
     {
+        if(lifeText == null)
+        {
+            Debug.LogError("ERROR: Text variable empty, please assign text object in inspector");
+        }
+
         currentLives = maxLives;
+        UpdateText();
     }
 
     public void removeLife(int livesLost)
     {
         currentLives -= livesLost;
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        lifeText.text = $"<3 : {currentLives} / {maxLives}";
     }
 }
