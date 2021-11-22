65355fb3121cedfe4701e7ac211f45247e71bf7b
ben-millar <c00236772@itcarlow.ie>
2021-11-22 12:05:01 +0000

    Added sprites, added bloons to Week1 build

--- Assets/Scripts/Bloon.cs
+++ Assets/Scripts/Bloon.cs
@@ -21,25 +21,27 @@ public class Bloon : MonoBehaviour
     private static Dictionary<int, Tier> _bloonTiers = new Dictionary<int, Tier>()
     {
         { 0, new Tier(1.0f, Color.red) },
-        { 1, new Tier(1.4f, Color.blue) },
+        { 1, new Tier(1.4f, new Color(0.4f,0.4f,1.0f)) },
         { 2, new Tier(1.8f, Color.green) },
         { 3, new Tier(3.2f, Color.yellow) },
         { 4, new Tier(3.5f, new Color(1.0f,0.5f,0.75f)) }
     };
 
-    private int _currentTier;
+    [Tooltip("Sets the tier of the bloon: MIN 0, MAX 4")]
+    [Range(0,4)]
+    public int _currentTier;
     private Rigidbody2D _rb;
     private SpriteRenderer _spr;
 
     // Unit vector representing our velocity
-    private Vector2 _velocity = new Vector2 { x = 2.0f, y = 0 }; // TODO: Change this; set to 2,0 for testing.
+    private Vector2 _velocity = new Vector2 { x = 0.5f, y = 0.0f }; // TODO: Change this; set to 0.5,0 for testing.
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _spr = GetComponent<SpriteRenderer>();
 
-        Spawn(4); // TODO: Change this; spawns tier 4 balloon for testing.
+        Spawn(_currentTier);
     }
 
     ////////////////////////////////////////////////////////////
@@ -63,6 +65,8 @@ public class Bloon : MonoBehaviour
     {
         float speed = _bloonTiers[_currentTier].speed;
         _rb.velocity = _velocity * speed;
+
+        Debug.Log(_rb.velocity);
     }
 
     ////////////////////////////////////////////////////////////
