1040e397f074c488cc16d9a54d484654ffb79ab5
HansMeulblok <hansmeulblok4@gmail.com>
2021-11-22 12:20:35 +0000

    Life recude and life tests

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField] public int maxLives;
+    [HideInInspector] public int currentLives;
+
+    void start()
+    {
+        currentLives = maxLives;
+    }
+
+    public void removeLife(int livesLost)
+    {
+        currentLives -= livesLost;
+    }
+}
---
+++ Assets/Tests/LifeTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LifeTests
+    {
+            private GameObject lifeSystem;
+            Life lifeComponent;
+            // A Test behaves as an ordinary method
+            [SetUp]
+            public void SetUp()
+            {
+               lifeSystem = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/LifeSystem"));
+               lifeComponent = lifeSystem.GetComponent<Life>();
+            }
+
+            [UnityTest]
+            public IEnumerator ReduceLife()
+            {        
+                lifeSystem.GetComponent<Life>().removeLife(50);
+                yield return new WaitForSeconds(.5f);
+                UnityEngine.Assertions.Assert.IsTrue(lifeComponent.currentLives < lifeComponent.maxLives);
+            }
+        }
+}
