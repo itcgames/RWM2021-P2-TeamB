33d9ce2d81f6e77c3b01ec21b19d8ac12f25c68d
HansMeulblok <43777979+HansMeulblok@users.noreply.github.com>
2021-11-22 14:43:59 +0000

    [B-11] Feat life system (#5)
    * Life recude and life tests
    
    * added UI
    
    * fixed tests

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour
+{
+    [SerializeField] public Text lifeText;
+    [SerializeField] public int maxLives;
+    [HideInInspector] public int currentLives;
+
+    void Start()
+    {
+        if(lifeText == null)
+        {
+            Debug.LogError("ERROR: Text variable empty, please assign text object in inspector");
+        }
+
+        currentLives = maxLives;
+        UpdateText();
+    }
+
+    public void removeLife(int livesLost)
+    {
+        currentLives -= livesLost;
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        lifeText.text = $"{currentLives} / {maxLives}";
+    }
+}
---
+++ Assets/Tests/LifeTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LifeTests
+    {
+            private GameObject lifeSystem;
+            Life lifeComponent;
+            // A Test behaves as an ordinary method
+            [SetUp]
+            public void SetUp()
+            {
+               lifeSystem = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/LifeSystem"));
+               lifeComponent = lifeSystem.GetComponent<Life>();
+            }
+
+            [UnityTest]
+            public IEnumerator ReduceLife()
+            {        
+                lifeComponent.removeLife(23);
+                yield return new WaitForSeconds(.5f);
+                UnityEngine.Assertions.Assert.IsTrue(lifeComponent.currentLives < lifeComponent.maxLives);
+            }
+
+            [UnityTest]
+            public IEnumerator UpdateLifeTotal()
+            {        
+                string text = lifeComponent.lifeText.text;
+                lifeComponent.removeLife(2);
+                yield return new WaitForSeconds(.5f);
+                UnityEngine.Assertions.Assert.IsFalse(text == lifeComponent.lifeText.text);
+            }
+        }
+}
