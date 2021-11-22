3cf76a3e6cf0d407837a76a6b64e3b1548eb179c
HansMeulblok <hansmeulblok4@gmail.com>
2021-11-22 16:24:51 +0000

    Added endscreen and end condition

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void Restart()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
