a0f6a469bfc1c6ced9da1d4fa6cc0ff37a9ead7f
Aaron O Neill <45260706+Aaron0Neill@users.noreply.github.com>
2021-12-06 19:35:35 +0000

    [BUG FIX] dart tower (#14)
    * [BI-9] Feat dart tower (#13)
    
    * [BI-32] Task dart animation (#11)
    
    * adding Sprite animations
    
    * Added attack down left + attack down
    
    * Bottom 3 animations done
    
    * Created Prefab + 3 animations
    
    * Changed how the animation works
    
    * Added dart tower to buildscene
    
    * Added Dart to Dart Tower (#12)
    
    * Rebase with master
    
    * [BUG FIXING] fixing dart tower
    
    Dart tower didn't use the dart projectile
    dart projectile was untagged
    changed the connections on the animator

--- Assets/Scripts/BaseTower.cs
+++ Assets/Scripts/BaseTower.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(TargetingSystem))]
 public class BaseTower : MonoBehaviour
 {
-    float _waitToFire; // how long until I can fire
+    protected float _waitToFire; // how long until I can fire
     [Tooltip("how long I have to reload")]
-    [SerializeField] float _reloadTime = 1;
+    [SerializeField] protected float _reloadTime = 1;
 
     [Tooltip("how far the tower can see")] 
-    [SerializeField] float _range = 1; 
+    [SerializeField] protected float _range = 1; 
 
-    GameObject _target; // object I'm firing at 
-    TargetingSystem _targetingSystem;
+    protected GameObject _target; // object I'm firing at 
+    protected TargetingSystem _targetingSystem;
 
     [Tooltip("Projectile that the tower will fire")]
-    [SerializeField]GameObject _projectile; // what im firing
+    [SerializeField] protected GameObject _projectile; // what im firing
+
+    protected RangeDetection _targetSystem;
 
 
     void Awake()
@@ -28,12 +30,13 @@ public class BaseTower : MonoBehaviour
         child.transform.position = transform.position;
         RangeDetection rangeDetector = child.AddComponent<RangeDetection>();
         rangeDetector.setRange(_range);
-        rangeDetector.OnObjectDetected += objectDetected;
+        rangeDetector.OnObjectDetected += Fire;
+        _targetSystem = transform.GetChild(0).GetComponent<RangeDetection>();
     }
 
     private void OnDisable()
     {
-        transform.GetChild(0).GetComponent<RangeDetection>().OnObjectDetected -= objectDetected;
+        transform.GetChild(0).GetComponent<RangeDetection>().OnObjectDetected -= Fire;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -50,24 +53,18 @@ public class BaseTower : MonoBehaviour
             _waitToFire -= Time.deltaTime;
     }
 
-    public void objectDetected(GameObject t_obj)
-    {
-        _target = t_obj;
-        Fire();
-    }
-
     public virtual void Fire()
     {
         if (_waitToFire <= 0)
         {
             Vector2 velocity = new Vector2();
             _waitToFire = _reloadTime;
-            if (_target != null)
+            if (_targetSystem.targets[0] != null)
                 velocity = _targetingSystem.getVelocity(_target.transform.position, transform.position);
             if (_projectile != null)
             {
-                Debug.Log(velocity);
-                GameObject go = Instantiate(_projectile, transform.position, Quaternion.identity, transform);
+                //Debug.Log(Mathf.Atan2(velocity.y, velocity.x) * 180 / Mathf.PI);
+                GameObject go = Instantiate(_projectile, transform.position, Quaternion.identity);
                 go.GetComponent<BaseProjectile>().Move(velocity);                
             }
         }
