046ecee16167b9449906859986e731660421909d
Aaron O Neill <45260706+Aaron0Neill@users.noreply.github.com>
2021-12-06 17:13:35 +0000

    [BI-9] Feat dart tower (#13)
    * [BI-32] Task dart animation (#11)
    
    * adding Sprite animations
    
    * Added attack down left + attack down
    
    * Bottom 3 animations done
    
    * Created Prefab + 3 animations
    
    * Changed how the animation works
    
    * Added dart tower to buildscene
    
    * Added Dart to Dart Tower (#12)
    
    * Rebase with master

---
+++ Assets/Scripts/DartTower.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartTower : BaseTower
+{
+
+    Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.SetInteger("attackAngle", 1);
+    }
+
+    public override void Fire()
+    {
+        if (_waitToFire <= 0)
+        {
+            _animator.SetTrigger("Attack");
+            Vector2 velocity = new Vector2();
+            _waitToFire = _reloadTime;
+            if (_targetSystem.targets.Count > 0)
+                velocity = _targetingSystem.getVelocity(_targetSystem.targets[0].transform.position, transform.position);
+            if (_projectile != null)
+            {
+                GameObject go = Instantiate(_projectile, transform.position, Quaternion.identity);
+                go.GetComponent<BaseProjectile>().Move(velocity);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_waitToFire > -0.1f)
+            _waitToFire -= Time.deltaTime;
+        else
+            _animator.SetTrigger("Idle");
+
+        if (_targetSystem.targets.Count > 0)
+        {
+            Vector2 diff = _targetSystem.targets[0].transform.position - transform.position;
+            float angle = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg + 90.0f;
+            transform.eulerAngles = new Vector3(0, 0, angle);
+        }
+
+    }
+}
