804f7e2333dc07f7ff9f999d933093254a09a6f1
HansMeulblok <hansmeulblok4@gmail.com>
2021-12-06 11:59:34 +0000

    Added UI, started on purchase menu code accidently

---
+++ Assets/PurchaseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseMenu : MonoBehaviour
+{
+    public int money;
+    [SerializeField] int dartMonkeyPrice;
+    [SerializeField] int tackShooterPrice;
+    [SerializeField] int CanonPrice;
+
+    public void PurchaseTower(string tower)
+    {
+        switch (tower)
+        {
+            case "TackShooter" :
+            break;
+
+            case "DartMonkey" :
+            break;
+
+            case "Canon" :
+            break;
+            
+            default:
+        }
+    }
+}
