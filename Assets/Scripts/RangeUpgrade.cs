dee741f0644e3c1b4697fa952f320032dab9cdea
Aaron O Neill <45260706+Aaron0Neill@users.noreply.github.com>
2021-12-07 14:27:19 +0000

    Added level system + range upgrade + rate of fire upgrade (#15)

---
+++ Assets/Scripts/RangeUpgrade.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeUpgrade : Ability
+{
+    [Tooltip("First index of the array is level 2, default values start at level 1")]
+    public float[] rangePerLevel;
+
+    public override void execute(GameObject caller)
+    {
+        Debug.Log("Upgrade");
+        int level = caller.GetComponent<EntityLeveling>().getLevel();
+        BaseTower tower = caller.GetComponent<DartTower>();
+        if (level < rangePerLevel.Length)
+            tower.upgradeRange(rangePerLevel[level]);
+    }
+}
